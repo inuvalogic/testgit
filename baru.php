55194c15488e041441cfdb5e74b66aef1e581c97
tim lain <inuvalogic@gmail.com>
2022-11-24 08:30:23 +0700

    diubah tim lain

--- baru.php
+++ baru.php
@@ -7,6 +7,18 @@
     <title>Document</title>
 </head>
 <body>
-    
+
+diubah tim lain
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+  <strong></strong> 
+</div>
+
+<script>
+  $(".alert").alert();
+</script>
+
 </body>
 </html>
